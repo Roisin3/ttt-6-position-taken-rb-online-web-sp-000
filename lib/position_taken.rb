5f9d76b10c77ba14520b5ff4279f9538f8e09538
Michael Lynch <michael@michaelrlynch.com>
2019-10-04 23:45:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,6 @@
 # code your #position_taken? method here!
+=begin
+<<<<<<< HEAD
 taken = nil
    if (board[index] ==  " " || board[index] == "" || board[index] == nil)
      taken = false
@@ -6,3 +8,12 @@ taken = nil
      taken = true
    end
    taken
+=end
+def position_taken? (board, index)
+  if board[index - 1] != "" || board[index -1] != " " || board[index - 1] == nil
+    return false
+  else
+    return true
+  end
+end
+# >>>>>>> 17505d14ec5a7dae76a4dad9c842af3a5af708ec
